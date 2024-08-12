c6e12c65c5dbce2953311c0498f20433c23c237b
joao victor <128156864+joaovictordocarmo@users.noreply.github.com>
2024-08-12 09:03:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//                     0  1   2   3   4   5
+const precosLivros = [25, 15, 30, 50, 45, 20];
+
+let atual = 0;
+let maisBarato = 0;
+
+for(let atual = 0; atual<precosLivros.length; atual++){
+    if(precosLivros[atual]<precosLivros[maisBarato]){
+        maisBarato = atual
+    }
+}
+console.log(`o livro mais barato custa ${precosLivros[maisBarato]}`)
